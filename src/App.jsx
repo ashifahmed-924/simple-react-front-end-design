d6649afc56795ae5b8d2e76a22b589cd20fa9b5e
Ashif ahmed <159910065+ashifahmed-924@users.noreply.github.com>
2024-10-02 00:25:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import Background from "./Components/background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
+const App = () => {
+  let herodata = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give into",text2:"your passions"},
+  ]
+
+  const [heroCount,setHeroCount] = useState(0);
+  const [playStatus,setPlayStatus] = useState(false);
+ 
+  useEffect( ()=>{
+    setInterval(()=>{
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 3000);
+  },[])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar/>
+      <Hero
+        setPlayStatus = {setPlayStatus}
+        herodata = {herodata[heroCount]}
+        heroCount = {heroCount}
+        setHeroCount = {setHeroCount}
+        playStatus = {playStatus}
+      />
+      </div>
+  )
+
+}
+
+export default App 
